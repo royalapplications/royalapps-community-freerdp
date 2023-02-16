766a4e219e3cfa4535f065dd4705272c2bbd1c47
Stefan Koell <stefan.koell@royalapps.com>
2023-02-16 09:27:37 +0100

    focus issue (WIP)

---
+++ src/RoyalApps.Community.FreeRdp.WinForms/WindowHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace RoyalApps.Community.FreeRdp.WinForms;
+
+internal class WindowHelper
+{
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+    
+    public static IntPtr GetFreeRdpWindow(IntPtr parentWindowHandle)
+    {
+        var returnHandle = IntPtr.Zero;
+        PInvoke.EnumChildWindows(new HWND(parentWindowHandle), (hWnd, _) =>
+        {
+            returnHandle = hWnd;
+            var sb = new StringBuilder();
+            GetClassName(hWnd, sb, 7);
+            return !sb.ToString().Equals("FREERDP", StringComparison.CurrentCultureIgnoreCase);
+        }, new LPARAM());
+        
+        return returnHandle;
+    }
+    
+    public static void SendFocusMessage(IntPtr hWnd)
+    {
+        if (hWnd == IntPtr.Zero)
+            return;
+
+        PInvoke.SendMessage(new HWND(hWnd), PInvoke.WM_SETFOCUS, new WPARAM(0), new LPARAM(0));
+    }
+}
