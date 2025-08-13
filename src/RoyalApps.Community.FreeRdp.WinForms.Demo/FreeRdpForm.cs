d19dd463edf311f71e533c71c869402e1b4372aa
nil4 <nil4@users.noreply.github.com>
2025-08-13 07:51:59 +0200

    Add .NET 10 target
    Not ready for release yet (RC1 needed for go-live)
    but sufficient for pre-release testing.
    
    Replace obsolete `From.Closing` -> `Form.FormClosing`

--- src/RoyalApps.Community.FreeRdp.WinForms.Demo/FreeRdpForm.cs
+++ src/RoyalApps.Community.FreeRdp.WinForms.Demo/FreeRdpForm.cs
@@ -30,7 +30,7 @@ public partial class FreeRdpForm : Form
             FormBorderStyle = FormBorderStyle.SizableToolWindow,
             StartPosition = FormStartPosition.CenterParent
         };
-        _form.Closing += (_, args) =>
+        _form.FormClosing += (_, args) =>
         {
             _form.Hide();
             args.Cancel = true;
